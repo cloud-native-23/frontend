64f29b299516a9d8e083a1dcb7a12922479010a7
Deru <deru.yang0086@gmail.com>
2023-12-14 13:57:00 +0800

    feat: Add fetchVenueInfo API test in editVenue

--- src/__tests__/admin/editVenue.test.js
+++ src/__tests__/admin/editVenue.test.js
@@ -1,5 +1,7 @@
 import { describe, jest, test, expect } from "@jest/globals"; 
 import { render, screen } from "@testing-library/react";
+import getConfig from "next/config";
+import axios from "axios";
 import EditVenue from "../../pages/admin/editVenue";
 
 describe("EditVenue testing", () => {
@@ -13,5 +15,23 @@ describe("EditVenue testing", () => {
 		render(<EditVenue />);
 		const navbar = screen.queryByTestId("navbar");
 		expect(navbar).toBeInTheDocument();
+	});
+
+	test("When fetchVenueInfo API is successful", async () => {
+		jest.mock("axios");
+		const mock = jest.spyOn(axios, "post");
+
+		const {
+			publicRuntimeConfig: { apiRoot },
+		} = getConfig();
+
+		const params = {
+			stadium_id: 1,
+		};
+		await axios.post(`${apiRoot}/api/v1/stadium/info`, {}, { params });
+
+		// then
+		expect(apiRoot).toBe("http://localhost:8000");
+		expect(mock).toHaveBeenCalledWith(`${apiRoot}/api/v1/stadium/info`, {}, { params });
 	});
 });
